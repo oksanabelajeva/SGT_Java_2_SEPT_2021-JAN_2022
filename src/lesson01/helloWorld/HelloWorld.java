b9b2b20b467b06f576644931037b8e4599fd620e
Oxana <oksanabelajeva@inbox.lv>
2021-10-04 21:07:04 +0300

    Initial commit

---
+++ src/lesson01/helloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+package lesson01.helloWorld;
+
+import lesson01.helloWorldLibrary.WorldLibrary;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        WorldLibrary worldLibrary = new WorldLibrary();
+        System.out.println(worldLibrary.getName() + ", welcome to JAVA2!");
+
+        worldLibrary.setPlanetName("Earth");
+        System.out.println("I live on a planet named " + worldLibrary.getPlanetName() + ".");
+
+        worldLibrary.setPlanetName("Mars");
+        System.out.println("But now I've decided to go to " + worldLibrary.getPlanetName() + ".");
+
+
+    }
+}
---
+++ src/lesson01/helloWorldLibrary/WorldLibrary.java
@@ -0,0 +1,18 @@
+package lesson01.helloWorldLibrary;
+
+public class WorldLibrary {
+
+    private String planetName;
+
+    public String getName(){
+        return "Oxana";
+    }
+
+    public String getPlanetName() {
+        return planetName;
+    }
+
+    public void setPlanetName(String planetName) {
+        this.planetName = planetName;
+    }
+}
