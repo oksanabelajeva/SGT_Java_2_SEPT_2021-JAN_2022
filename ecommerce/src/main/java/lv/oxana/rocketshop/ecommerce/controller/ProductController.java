b444c81476357fbe3d91960980a162daf0be3fd0
Oxana <oksanabelajeva@inbox.lv>
2021-10-29 21:44:54 +0300

    Lessons till lesson 14 (including) added.

---
+++ ecommerce/src/main/java/lv/oxana/rocketshop/ecommerce/controller/ProductController.java
@@ -0,0 +1,20 @@
+package lv.oxana.rocketshop.ecommerce.controller;
+
+import lv.oxana.rocketshop.ecommerce.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    ProductServiceImpl productService;
+
+//    @GetMapping(value = { "", "/" })
+//    public Iterable<Product> getProducts() {
+//        return productService.getAllProducts();
+//    }
+}
---
+++ ecommerce/src/main/java/lv/oxana/rocketshop/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,6 @@
+package lv.oxana.rocketshop.ecommerce.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, Long> {
+}
