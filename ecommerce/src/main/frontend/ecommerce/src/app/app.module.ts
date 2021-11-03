50925bf17882619be276caed1d6be30ad139349d
Oxana <oksanabelajeva@inbox.lv>
2021-11-03 22:01:36 +0200

    Lesson 16 part 2 added.

---
+++ ecommerce/src/main/frontend/ecommerce/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { EcommerceComponent } from './ecommerce/ecommerce.component';
+import { ProductsComponent } from './ecommerce/products/products.component';
+import { OrdersComponent } from './ecommerce/orders/orders.component';
+import { ShoppingCartComponent } from './ecommerce/shopping-cart/shopping-cart.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EcommerceComponent,
+    ProductsComponent,
+    OrdersComponent,
+    ShoppingCartComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
